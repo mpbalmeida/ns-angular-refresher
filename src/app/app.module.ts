4c91ea0444ced26e146c83c0a86a23ecce749f37
marcos.almeida <me@marcosalmeida.dev>
2022-12-01 00:13:10 +0100

    feat: execute method with local reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonInputComponent } from './persons/person-input/person-input.component';
 import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonsComponent
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/persons/person-input/person-input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+export class PersonInputComponent {
+  onCreateUser(personName: string) {
+    console.log(`person created ${personName}`);
+  }
+
+}
