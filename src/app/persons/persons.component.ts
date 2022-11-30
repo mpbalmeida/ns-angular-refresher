24bfb3ec475ff6cec8ad5cad1e13fa04a708dba8
marcos.almeida <me@marcosalmeida.dev>
2022-12-01 00:01:22 +0100

    feat: iterate list

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-persons',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./persons.component.css']
 })
 export class PersonsComponent {
-
+  @Input() personList: string[] = [];
 }
