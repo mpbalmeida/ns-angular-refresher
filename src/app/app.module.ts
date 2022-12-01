b44aba0e2f8040223ed899a30d006fac7901c6b0
marcos.almeida <me@marcosalmeida.dev>
2022-12-01 22:51:16 +0100

    feat: add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PersonInputComponent } from './persons/person-input/person-input.component';
@@ -13,6 +14,7 @@ import { PersonsComponent } from './persons/persons.component';
     PersonInputComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
   ],
