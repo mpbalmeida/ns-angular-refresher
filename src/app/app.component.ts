c8bc503a7e43d4382aa1b5fa81ad5fcbc0f31807
marcos.almeida <me@marcosalmeida.dev>
2022-12-01 22:41:01 +0100

    feat: add person to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  onPersonCreated(name: string) {
+    this.persons.push(name);
+  }
   persons: string [] = ['Marcos', 'Ann', 'Peter'];
 }
--- src/app/persons/person-input/person-input.component.ts
+++ src/app/persons/person-input/person-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-person-input',
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class PersonInputComponent {
 
+  @Output() createdPerson = new EventEmitter<string>();
   enteredPersonName: string = '';
 
   onCreateUser() {
     console.log(`person created ${this.enteredPersonName}`);
+    this.createdPerson.emit(this.enteredPersonName);
     this.enteredPersonName = '';
   }
 
